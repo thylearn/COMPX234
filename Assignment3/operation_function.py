fe8ffa43de5bfeb80a30cf59f809801d4810e002
thylearn <2395172226@qq.com>
2025-04-27 21:52:30 +0800

    Add read(k) method with locking

--- Assignment3/operation_function.py
+++ Assignment3/operation_function.py
@@ -9,4 +9,10 @@ class Operations:
         # use lock
         self.lock = threading.Lock()
     
-    
+    def read(self, k):
+        """
+        if k exists return v
+        else return none
+        """
+        with self.lock:
+            return self.tuple_space.get(k, None)
