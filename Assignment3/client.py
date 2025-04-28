7c5b007fdff8ae18221534562b7d10c81e4a4159
thylearn <2395172226@qq.com>
2025-04-28 16:14:51 +0800

    Add main() and parse arguments

---
+++ Assignment3/client.py
@@ -0,0 +1,12 @@
+# client.py - client program to interact with server
+import socket
+import sys
+import os
+
+def main():
+    local_host = sys.argv[1]
+    port = int(sys.argv[2])
+    text_file = sys.argv[3]
+
+if __name__ == '__main__':
+    main()
