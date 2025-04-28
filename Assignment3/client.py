6e54dada5e2bb67a1e5ec7a9ca3736392e8bc6a3
thylearn <2395172226@qq.com>
2025-04-28 16:17:32 +0800

    Implement TCP socket connection

--- Assignment3/client.py
+++ Assignment3/client.py
@@ -7,6 +7,9 @@ def main():
     local_host = sys.argv[1]
     port = int(sys.argv[2])
     text_file = sys.argv[3]
-
+    
+    # create a TCP connection and connect to the server
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as soc:
+        soc.connect((local_host, port))
 if __name__ == '__main__':
     main()
