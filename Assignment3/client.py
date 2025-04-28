68abf3bcd6765e40b0e5ede4127cdb97ba6d8585
thylearn <2395172226@qq.com>
2025-04-28 16:41:29 +0800

    Open input file

--- Assignment3/client.py
+++ Assignment3/client.py
@@ -8,8 +8,20 @@ def main():
     port = int(sys.argv[2])
     text_file = sys.argv[3]
     
+    if not os.path.exists(text_file):
+        # can't find the file
+        return
+
     # create a TCP connection and connect to the server
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as soc:
         soc.connect((local_host, port))
+
+        # open file 
+        with open(text_file, "r", encoding='utf-8') as file:
+            for lines in file:
+                line = lines.strip()
+                if not line:
+                    continue
+
 if __name__ == '__main__':
     main()
