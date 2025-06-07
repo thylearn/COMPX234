f55f378b4c00711d4c1493a3a9ffc7ef3b7d1c05
thylearn <2395172226@qq.com>
2025-06-07 18:53:28 +0800

    Create UDP socket for sending DOWNLOAD requests

--- Assignment4/client_UDP.py
+++ Assignment4/client_UDP.py
@@ -1,4 +1,5 @@
 import sys
+import socket
 
 # Parse user input
 def parse_arguments():
@@ -10,12 +11,20 @@ def parse_arguments():
     file_path = sys.argv[3]
     return host, port, file_path
 
-def read_file_list(file_path):
+# Read file method
+def read_file(file_path):
     try:
         with open(file_path) as f:
             file = f.read().splitlines()
     except FileNotFoundError:
-        print(f"[ERROR] Cannot open file list: {file_path}")
+        print(f"Cannot open file list: {file_path}")
         sys.exit(1)
-    print("[INFO] Files to download:", file)
+    print("Files to download:", file)
     return file
+
+# Create UDP socket
+def control_socket(host, port):
+    control_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    control_address = (host, port)
+    print("Control socket created")
+    return control_socket, control_address
