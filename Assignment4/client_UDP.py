e09af0dde6f90cba8dab86c74614f45173e04471
thylearn <2395172226@qq.com>
2025-06-07 15:49:00 +0800

    Add logic to read filenames from the given file

--- Assignment4/client_UDP.py
+++ Assignment4/client_UDP.py
@@ -9,3 +9,13 @@ def parse_arguments():
     port = int(sys.argv[2])
     file_path = sys.argv[3]
     return host, port, file_path
+
+def read_file_list(file_path):
+    try:
+        with open(file_path) as f:
+            file = f.read().splitlines()
+    except FileNotFoundError:
+        print(f"[ERROR] Cannot open file list: {file_path}")
+        sys.exit(1)
+    print("[INFO] Files to download:", file)
+    return file
